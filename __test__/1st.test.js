1432cd6d93f63e5708b570aaa695206fa49f61c6
Listar <shnap2010@yandex.ru>
2019-05-17 16:11:52 +0500

    #2 Сконфигурировать Jest в проекте
    Добавлены дефолтные тесты ?

---
+++ __test__/1st.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import TestComponent from './index';
+
+describe('test', () => {
+  it('test1', () => {
+    const wrapper = shallow(<TestComponent />);
+    expect(wrapper.find('h1')).toMatchSnapshot();
+  });
+
+  it('test2', () => {
+    const wrapper = shallow(<TestComponent />);
+    expect(wrapper.find('h1')).toHaveLength(1);
+  });
+});
---
+++ __test__/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './style.css';
+/**
+ * Test
+ * @return {*} any any
+ */
+function App () {
+  return (
+    <div className='App'>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+export default App;
