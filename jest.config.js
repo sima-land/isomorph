9c6211cce28e8234334feab7ab15ad21468d8556
Listar <shnap2010@yandex.ru>
2019-05-17 13:07:04 +0500

    ISOMORPH-2 Сконфигурировать Jest в проекте
    Добавлены бабель плагины

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   setupFiles: ['./jestConfig.js'],
+  transform: {
+    '^.+\\.(js|jsx)?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
 };
