22bd3a27efd6d6be41f2e3408a0d412211bf428c
Дмитрий Шишкин <shishkin_da@ekb.sima-land.ru>
2019-05-17 16:21:53 +0500

    #1 Настроить сборку проекта

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const { src, dest, series } = require('gulp');
 const del = require('del');
 
 const clean = () => del(['build']);
-const move = () => src('./src/**/*').pipe(dest('build'));
+const move = () => src(['./src/**/*', '!**/*.test.js']).pipe(dest('build'));
 
 const build = series(clean, move);
 
