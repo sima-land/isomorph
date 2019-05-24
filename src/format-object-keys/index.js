397d73db4875ccbd7fefa9a8f6a86787a361e9ec
Дмитрий Шишкин <shishkin_da@ekb.sima-land.ru>
2019-05-24 18:04:56 +0500

    #7 Реализовать механизм для конфигурирования приложения

--- src/format-object-keys/index.js
+++ src/format-object-keys/index.js
@@ -2,15 +2,15 @@ import { camelCase, isPlainObject } from 'lodash';
 
 /**
  * Переводит ключи объекта в camelCase
- * @param {Object} obj Объект ключи которого нужно отформатировать
+ * @param {Object} object Объект ключи которого нужно отформатировать
  * @return {Object} Отформатированный объект
  */
-export default function formatObjectKeys (obj) {
-  if (!isPlainObject(obj)) {
-    throw new TypeError('Параметр "obj" должен быть объеком');
+export default function formatObjectKeys (object) {
+  if (!isPlainObject(object)) {
+    throw new TypeError('Параметр "object" должен быть объеком');
   }
 
-  return Object.entries(obj).reduce((formattedObject, current) => {
+  return Object.entries(object).reduce((formattedObject, current) => {
     const [key, value] = current;
     const formattedKey = camelCase(key);
 
