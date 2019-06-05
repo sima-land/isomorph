76ac81c2c050cf6e3197308ff5115a25f2845b40
Дмитрий Шишкин <shishkin_da@ekb.sima-land.ru>
2019-06-05 18:02:58 +0500

    #9 Реализовать логгирование в приложении

--- src/logger/create-logger-middleware/index.test.js
+++ src/logger/create-logger-middleware/index.test.js
@@ -1,4 +1,6 @@
 import createLoggerMiddleware from './index';
+import createPinoLogger from '../helpers/config-pino-logger';
+import createPinoInstance from '../helpers/create-pino-instance';
 
 const config = { version: 1 };
 const httpHelpers = {
@@ -7,11 +9,27 @@ const httpHelpers = {
   getStatus: jest.fn(),
 };
 
+jest.mock('../helpers/config-pino-logger', () => jest.fn());
+jest.mock('../helpers/create-pino-instance', () => jest.fn());
+
 describe('createLoggerMiddleware', () => {
   it('works correctly', () => {
     createLoggerMiddleware({
       config,
       httpHelpers,
     });
+
+    expect(createPinoLogger).toHaveBeenCalledWith({
+      logger: createPinoInstance({ config }),
+      staticData: {
+        version: config.version,
+      },
+      dynamicData: {
+        remote_ip: httpHelpers.getXClientIp,
+        method: httpHelpers.getMethod,
+        status: httpHelpers.getStatus,
+      },
+    });
+    expect(createPinoInstance).toHaveBeenCalledWith({ config });
   });
 });
