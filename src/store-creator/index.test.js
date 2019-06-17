e3f7487dd286c801941d7bfb0bd02f12be3f3dae
Max Bagaev <bagaev_md@ekb.sima-land.ru>
2019-06-17 09:11:06 +0500

    #13 Реализовать сервис для создания store приложения

--- src/store-creator/index.test.js
+++ src/store-creator/index.test.js
@@ -15,4 +15,15 @@ describe('function storeCreator()', () => {
     expect(applyMiddleware).toHaveBeenCalledWith([]);
     expect(compose).toHaveBeenCalledWith(1);
   });
+
+  it('storeCreator() works properly with default compose', () => {
+    const reducer = jest.fn();
+    storeCreator({
+      initialState: {},
+      reducer,
+      middleware: [],
+    });
+    expect(createStore).toHaveBeenCalledWith(reducer, {}, 1);
+    expect(applyMiddleware).toHaveBeenCalledWith([]);
+  });
 });
