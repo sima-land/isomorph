9f4ee737585545d2b5811443eef001c078266c8a
Ivan Popov <popov_i_sima@outlook.com>
2019-11-20 15:53:10 +0500

    feat(createReducer): Перенесены хелперы для создания редьюсеров экшенов обработки запросов из проекта карточки товара.

---
+++ src/helpers/redux/constants.js
@@ -0,0 +1,10 @@
+/**
+ * Статусы состояния данных.
+ * @type {Object}
+ */
+export const STATUSES = Object.freeze({
+  initial: 'initial',
+  success: 'success',
+  fetching: 'fetching',
+  failure: 'failure',
+});
