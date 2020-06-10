bdf374add4b9d2803e879f935a886268442fdff8
rotanov_ps <rotanov_ps@ekb.sima-land.ru>
2020-06-10 13:16:35 +0500

    #90 Добавить в createTracingMiddleware установку тегов с информацией о заголовках запроса, роуте ... - Добавил создание сервисов из функций трассировки, обработчик завершения спана; - Добавил добавление к спану контекста, в котором обрабатывается запрос к сервису; - Дополнил пример использования.

--- src/helpers/tracer/__test__/index.test.js
+++ src/helpers/tracer/__test__/index.test.js
@@ -1,5 +1,14 @@
-import { getTracer, traceIncomingRequest, createTracingMiddleware, getSpanContext } from '..';
-import { initTracerFromEnv, tracer } from '../../../../__mocks__/jaeger-client';
+import {
+  getTracer,
+  traceIncomingRequest,
+  _createTracingMiddleware,
+  getSpanContext,
+  _deptToArg,
+  _getRequestContext,
+  createSpanCreator,
+  spanFinishHandler,
+} from '..';
+import { initTracerFromEnv, tracer, testSpan } from '../../../../__mocks__/jaeger-client';
 import { FORMAT_HTTP_HEADERS } from 'opentracing';
 import { createObserveMiddleware } from '../../../observe-middleware/';
 
@@ -34,34 +43,44 @@ describe('getTracer()', () => {
 });
 
 describe('traceIncomingRequest()', () => {
-  it('traceIncomingRequest() calls method tracer.startSpan', () => {
-    const httpRequest = { headers: 1 };
+  const httpRequest = { headers: 1 };
+  it('should calls method tracer.startSpan', () => {
     traceIncomingRequest(tracer, 'key', httpRequest);
     expect(tracer.startSpan).toHaveBeenCalledTimes(1);
     expect(tracer.startSpan).toHaveBeenCalledWith('key', {});
     expect(tracer.extract).toHaveBeenCalledTimes(1);
     expect(tracer.extract).toHaveBeenCalledWith(FORMAT_HTTP_HEADERS, 1);
   });
+
+  it('should calls span.addTags method with payload', () => {
+    traceIncomingRequest(tracer, 'key', httpRequest, { test: 'test' });
+    expect(testSpan.addTags).toBeCalledWith({ test: 'test' });
+  });
+  it('should calls span.addTags method with empty object', () => {
+    traceIncomingRequest(tracer, 'key', httpRequest);
+    traceIncomingRequest(tracer, 'key', httpRequest, 'badPayload');
+    expect(testSpan.addTags).toHaveBeenNthCalledWith(1, {});
+    expect(testSpan.addTags).toHaveBeenNthCalledWith(2, {});
+  });
 });
 
-describe('createTracingMiddleware()', () => {
+describe('_createTracingMiddleware()', () => {
   it('should return result of createObserveMiddleware call', () => {
     const testValue = Symbol('test');
     createObserveMiddleware.mockReturnValue(testValue);
     expect(createObserveMiddleware).toHaveBeenCalledTimes(0);
-    expect(createTracingMiddleware()).toBe(testValue);
+    expect(_createTracingMiddleware()).toBe(testValue);
     expect(createObserveMiddleware).toHaveBeenCalledTimes(1);
   });
   it('should call createObserveMiddleware correctly', () => {
-    createTracingMiddleware();
+    _createTracingMiddleware();
     expect(typeof createObserveMiddleware.mock.calls[0][0].onStart).toBe('function');
     expect(typeof createObserveMiddleware.mock.calls[0][0].onFinish).toBe('function');
   });
   it('should pass properly worked functions to createObserveMiddleware', () => {
-    const testSpan = { testSpan: true };
     const spanCreator = jest.fn(() => testSpan);
-    const spanFinishHandler = jest.fn();
-    createTracingMiddleware(spanCreator, spanFinishHandler);
+    const finishHandler = jest.fn();
+    _createTracingMiddleware(spanCreator, finishHandler);
     const requestStartHandler = createObserveMiddleware.mock.calls[0][0].onStart;
     const requestFinishHandler = createObserveMiddleware.mock.calls[0][0].onFinish;
 
@@ -73,16 +92,15 @@ describe('createTracingMiddleware()', () => {
     expect(spanCreator).toHaveBeenCalledTimes(1);
     expect(spanCreator).toHaveBeenCalledWith(testRequest, testResponse);
 
-    expect(spanFinishHandler).toHaveBeenCalledTimes(0);
+    expect(finishHandler).toHaveBeenCalledTimes(0);
     requestFinishHandler(0, testRequest, testResponse);
-    expect(spanFinishHandler).toHaveBeenCalledTimes(1);
-    expect(spanFinishHandler).toHaveBeenCalledWith(testRequest, testResponse, testSpan);
+    expect(finishHandler).toHaveBeenCalledTimes(1);
+    expect(finishHandler).toHaveBeenCalledWith(testRequest, testResponse, testSpan);
   });
   it('should handle options argument', () => {
-    const testSpan = { testSpan: true };
     const spanCreator = jest.fn(() => testSpan);
-    const spanFinishHandler = jest.fn();
-    createTracingMiddleware(spanCreator, spanFinishHandler, {
+    const finishHandler = jest.fn();
+    _createTracingMiddleware(spanCreator, finishHandler, {
       spanKey: 'testSpanKey',
     });
     const requestStartHandler = createObserveMiddleware.mock.calls[0][0].onStart;
@@ -94,7 +112,7 @@ describe('createTracingMiddleware()', () => {
     expect(testResponse.locals.testSpanKey).toBe(testSpan);
   });
   it('should create handlers that works without arguments', () => {
-    createTracingMiddleware();
+    _createTracingMiddleware();
     const requestStartHandler = createObserveMiddleware.mock.calls[0][0].onStart;
     const requestFinishHandler = createObserveMiddleware.mock.calls[0][0].onFinish;
 
@@ -146,5 +164,102 @@ describe('getSpanContext', () => {
     };
     const context = getSpanContext({ response });
     expect(context).toBe(null);
+  });
+});
+
+describe('_deptToArg()', () => {
+  it('should work correctly', () => {
+    const dept = { createSpan: 'createSpan', onSpanFinish: 'onSpanFinish' };
+    expect(_deptToArg(dept)).toEqual(['createSpan', 'onSpanFinish']);
+  });
+});
+
+describe('_getRequestContext()', () => {
+  const request = {
+    originalUrl: '/foo',
+    headers: {
+      'Simaland-Params': 'test',
+    },
+
+    /**
+     * Возвращает содержимое заголовка.
+     * @param {string} propName Имя заголовка.
+     * @return {string} Содержимое.
+     */
+    get (propName) {
+      return this.headers[propName];
+    },
+  };
+
+  it('should return correct context', () => {
+    expect(_getRequestContext(request, { test: 'test', redisPassword: 'secret' })).toEqual({
+      'request.path': '/foo',
+      'request.headers.Simaland-Params': 'test',
+      'app.config': {
+        test: 'test',
+        redisPassword: '[Filtered]',
+      },
+    });
+  });
+
+  it('should return correct context if config not object', () => {
+    expect(_getRequestContext(request)).toEqual({
+      'request.path': '/foo',
+      'request.headers.Simaland-Params': 'test',
+      'app.config': '',
+    });
+
+    expect(_getRequestContext(request, 'bad config')).toEqual({
+      'request.path': '/foo',
+      'request.headers.Simaland-Params': 'test',
+      'app.config': '',
+    });
+  });
+
+  it('should return correct context if Simaland-Params headers not defined', () => {
+    expect(_getRequestContext({ ...request, headers: {} }, { test: 'test' })).toEqual({
+      'request.path': '/foo',
+      'request.headers.Simaland-Params': '',
+      'app.config': { test: 'test' },
+    });
+  });
+
+  it('should return correct context if get method not a function', () => {
+    expect(_getRequestContext({ ...request, get: null }, { test: 'test' })).toEqual({
+      'request.path': '/foo',
+      'request.headers.Simaland-Params': '',
+      'app.config': { test: 'test' },
+    });
+  });
+});
+
+describe('createSpanCreator()', () => {
+  const instance = createSpanCreator({ jaegerTracer: tracer, config: { test: 'test' } });
+  it('should create function', () => {
+    expect(instance).toBeInstanceOf(Function);
+    expect(instance).toHaveLength(1);
+  });
+
+  describe('instance', () => {
+    const request = { headers: 1, originalUrl: '/test' };
+    it('should calls method tracer.startSpan', () => {
+      instance(request);
+      expect(tracer.startSpan).toHaveBeenCalledTimes(1);
+      expect(tracer.startSpan).toHaveBeenCalledWith('incoming-http-request', {});
+      expect(tracer.extract).toHaveBeenCalledTimes(1);
+      expect(tracer.extract).toHaveBeenCalledWith(FORMAT_HTTP_HEADERS, 1);
+      expect(testSpan.addTags).toBeCalledWith({
+        'app.config': { test: 'test' },
+        'request.headers.Simaland-Params': '',
+        'request.path': '/test',
+      });
+    });
+  });
+});
+
+describe('spanFinishHandler', () => {
+  it('handles finish correctly', () => {
+    const span = { finish: jest.fn() };
+    spanFinishHandler({}, {}, span);
   });
 });
