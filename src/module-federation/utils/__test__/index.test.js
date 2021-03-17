35d418034c7f101b154ffaade06ff5c5a4b5162b
Pavel Rotanov <rotanov_ps@ekb.sima-land.ru>
2021-03-17 10:55:34 +0500

    #118 Доработать createExternalConfig MF - Добавлены выбросы ошибок в скрипт загрузки;

--- src/module-federation/utils/__test__/index.test.js
+++ src/module-federation/utils/__test__/index.test.js
@@ -6,16 +6,22 @@ describe('createExternalConfig', () => {
       'service-name',
       'remote-entries',
       'containers'
-    )).toEqual(['promise new Promise(resolve => {',
+    )).toEqual(['promise new Promise((resolve, reject) => {',
       '  if (window[\'remote-entries\']) {',
       '    const scriptElement = document.createElement(\'script\');',
       '    scriptElement.onload = () => {',
       '      scriptElement.remove();',
       '      resolve(window[\'containers\'][\'service-name\']);',
       '    };',
+      '    scriptElement.onerror = () => {',
+      '      scriptElement.remove();',
+      '      reject(new Error(\'Failed loading remoteEntry for "service-name".\'));',
+      '    };',
       '    scriptElement.src = window[\'remote-entries\'][\'service-name\'];',
       '    scriptElement.async = true;',
       '    document.head.append(scriptElement);',
+      '  } else {',
+      '    reject(new ReferenceError(\'Object "remote-entries" unavailable.\'));',
       '  }',
       '})'].join('\n'));
   });
