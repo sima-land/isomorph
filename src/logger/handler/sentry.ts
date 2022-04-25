709287fdbb948e3be8b7001c545348a7b68c1d40
krutoo <dima.krutoo@yandex.ru>
2022-04-25 09:29:35 +0500

    #24
    - Logger: handler Sentry теперь умеет обрабатывать SentryError

--- src/logger/handler/sentry.ts
+++ src/logger/handler/sentry.ts
@@ -1,5 +1,6 @@
 import type { SentryLib } from '../../error-tracker/types';
 import type { LoggerEventHandler } from '../types';
+import { SentryError } from '../../error-tracker/utils';
 
 /**
  * Возвращает новый handler для logger'а для отправки событий в Sentry.
@@ -9,7 +10,29 @@ import type { LoggerEventHandler } from '../types';
 export function createSentryHandler(sentry: SentryLib): LoggerEventHandler {
   return event => {
     if (event.type === 'error') {
-      sentry.client.captureException(event.data);
+      const error = event.data;
+
+      if (error instanceof SentryError) {
+        const { level, context, extra } = error.data;
+
+        sentry.withScope(scope => {
+          if (level) {
+            scope.setLevel(level);
+          }
+
+          if (context) {
+            scope.setContext(context.key, context.data);
+          }
+
+          if (extra) {
+            scope.setExtra(extra.key, extra.data);
+          }
+
+          sentry.client.captureException(error);
+        });
+      } else {
+        sentry.client.captureException(error);
+      }
     }
   };
 }
