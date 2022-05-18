a7b668273e86881aed25f508789ef0224d66e2cf
krutoo <dima.krutoo@yandex.ru>
2022-05-18 13:54:50 +0500

    #24
    - правки публикации
    - правки lock-файла в examples/server
    - удален legacy-src
    - добавлена утиилита `sauce` для http-клиента

--- examples/server/readme.md
+++ examples/server/readme.md
@@ -6,8 +6,9 @@
 
 1. Перейти в директорию проекта `@sima-land/isomorph`
 1. Выполнить `npm install`
+1. Выполнить `npm run build`
 1. Выполнить `npm link`
-1. Выполнить `cd ./node_modules/react && npm link`
+1. Из директории проекта `@sima-land/isomorph` перейти в `node_modules/react`
 1. Выполнить `npm link`
 1. Перейти в директорию данного приложения
 1. Выполнить `npm install`
--- src/http-client/utils.ts
+++ src/http-client/utils.ts
@@ -1,6 +1,64 @@
 import { getServiceHeaders, getXClientIp } from '../http-server/utils';
 import type { Request } from 'express';
 import type { BaseConfig } from '../config/types';
+import type { AxiosInstance } from 'axios';
+import type { AxiosInstanceWrapper, MethodName } from 'middleware-axios';
+
+// @todo передавать заголовки
+export type SauceResponse<T = any> =
+  | {
+      ok: true;
+      data: T;
+    }
+  | {
+      ok: false;
+      error: any;
+    };
+
+export interface SauceMethod<K extends MethodName> {
+  <T>(...args: Parameters<AxiosInstanceWrapper[K]>): Promise<SauceResponse<T>>;
+}
+
+export interface Sauce {
+  readonly get: SauceMethod<'get'>;
+  readonly delete: SauceMethod<'delete'>;
+  readonly head: SauceMethod<'head'>;
+  readonly options: SauceMethod<'options'>;
+  readonly post: SauceMethod<'post'>;
+  readonly put: SauceMethod<'put'>;
+  readonly patch: SauceMethod<'patch'>;
+}
+
+/**
+ * Оборачивает экземпляр axios. Все методы обертки возвращают promise, который никогда не попадают в состояние rejected.
+ * @param instance Экземпляр axios или AxiosInstanceWrapper.
+ * @return Обертка.
+ */
+export function sauce(instance: AxiosInstance | AxiosInstanceWrapper): Sauce {
+  // eslint-disable-next-line require-jsdoc, jsdoc/require-jsdoc
+  function createMethod<Key extends MethodName>(methodName: Key) {
+    return async function <Data = any>(
+      ...args: Parameters<AxiosInstance[Key]>
+    ): Promise<SauceResponse<Data>> {
+      try {
+        const response = await instance[methodName](...(args as [any]));
+        return { ok: true, data: response.data };
+      } catch (error) {
+        return { ok: false, error };
+      }
+    };
+  }
+
+  return {
+    get: createMethod('get'),
+    delete: createMethod('delete'),
+    head: createMethod('head'),
+    options: createMethod('options'),
+    post: createMethod('post'),
+    put: createMethod('put'),
+    patch: createMethod('patch'),
+  };
+}
 
 /**
  * Формирует заголовки для исходящих запросов с сервера по соглашению.
