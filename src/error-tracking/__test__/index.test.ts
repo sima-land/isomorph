ce6b246bb4bbc122d7119071b0c46152789fb19f
krutoo <dima.krutoo@yandex.ru>
2022-06-02 16:28:30 +0500

    #24
    - дописаны тесты

--- src/error-tracking/__test__/index.test.ts
+++ src/error-tracking/__test__/index.test.ts
@@ -1,7 +1,13 @@
-import { SentryError } from '..';
+import { SentryError, SentryBreadcrumb } from '..';
 
 describe('SentryError', () => {
   it('name should be just "Error"', () => {
     expect(new SentryError('hello, world!').name).toBe('Error');
+  });
+});
+
+describe('SentryBreadcrumb', () => {
+  it('name should be just "Error"', () => {
+    expect(new SentryBreadcrumb({ message: 'some text' }).data).toEqual({ message: 'some text' });
   });
 });
