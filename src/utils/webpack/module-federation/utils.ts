93d769b53c16be69cafa79250a163abf32386103
barabaiiika <pavel.rtnv@gmail.com>
2022-09-14 11:22:23 +0500

    Доработать CustomModuleFederationPlugin - Доработан external promise для отложенной инициализации контейнера;

--- src/utils/webpack/module-federation/utils.ts
+++ src/utils/webpack/module-federation/utils.ts
@@ -19,26 +19,57 @@ export function createExternalConfig({
   containersGlobalKey: string;
   remoteEntryPath?: string;
 }) {
-  return `promise new Promise((resolve, reject) => {
-  if (window['${remoteEntriesGlobalKey}']) {
-    const scriptElement = document.createElement('script');
-    scriptElement.onload = () => {
-      scriptElement.remove();
-      resolve(window['${containersGlobalKey}']['${serviceName}']);
+  return `promise new Promise((resolveProxy, rejectProxy) => {
+  if (window["${remoteEntriesGlobalKey}"]) {
+    const proxy = {
+      get(request) {
+        return this.__installed
+          ? window["${containersGlobalKey}"]["${serviceName}"].get(request)
+          : new Promise((resolveRequest, rejectRequest) => {
+              const scriptElement = document.createElement("script");
+
+              scriptElement.onload = () => {
+                scriptElement.remove();
+                const container =
+                  window["${containersGlobalKey}"]["${serviceName}"];
+                try {
+                  container.init(this.__shareScope);
+                  this.__installed = true;
+                } catch (e) {}
+                resolveRequest(container.get(request));
+              };
+              
+              scriptElement.onerror = () => {
+                scriptElement.remove();
+                rejectRequest(
+                  new Error('Failed loading remoteEntry for "${serviceName}".')
+                );
+              };
+              
+              scriptElement.src = ${
+                remoteEntryPath
+                  ? `'${remoteEntryPath}'`
+                  : `window['${remoteEntriesGlobalKey}']['${serviceName}']`
+              };
+              
+              scriptElement.async = true;
+              document.head.append(scriptElement);
+            });
+      },
+      init(scope) {
+        this.__shareScope = scope;
+      },
     };
-    scriptElement.onerror = () => {
-      scriptElement.remove();
-      reject(new Error('Failed loading remoteEntry for "${serviceName}".'));
-    };
-    scriptElement.src = ${
-      remoteEntryPath
-        ? `'${remoteEntryPath}'`
-        : `window['${remoteEntriesGlobalKey}']['${serviceName}']`
-    };
-    scriptElement.async = true;
-    document.head.append(scriptElement);
+
+    for (const method in proxy) {
+      proxy[method] = proxy[method].bind(proxy);
+    }
+
+    resolveProxy(proxy);
   } else {
-    reject(new ReferenceError('Object "${remoteEntriesGlobalKey}" unavailable.'));
+    rejectProxy(
+      new ReferenceError('Object "${remoteEntriesGlobalKey}" unavailable.')
+    );
   }
-})`;
+});`;
 }
