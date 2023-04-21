0fa239e56d89de8eb1a35f4e0d3f6abda2d9301f
krutoo <dima.krutoo@yandex.ru>
2023-04-21 12:49:21 +0500

    #38
    - examples/server: восстановлен сервер метрик

--- examples/server/src/index.ts
+++ examples/server/src/index.ts
@@ -2,10 +2,14 @@ import { MainApp } from './di/apps/main';
 import { TOKEN } from './di/tokens';
 
 MainApp().invoke(
-  [TOKEN.appConfig, TOKEN.Known.logger, TOKEN.httpServer],
-  (config, logger, server) => {
-    server.listen(config.http.ports.main, () => {
+  [TOKEN.appConfig, TOKEN.Known.logger, TOKEN.httpServer, TOKEN.Known.Metrics.httpApp],
+  (config, logger, mainServer, metricsServer) => {
+    mainServer.listen(config.http.ports.main, () => {
       logger.info(`Server started on port ${config.http.ports.main}`);
+    });
+
+    metricsServer.listen(config.http.ports.metrics, () => {
+      logger.info(`Metrics app started on port ${config.http.ports.metrics}`);
     });
   },
 );
