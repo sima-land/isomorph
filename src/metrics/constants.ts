4ffae1ff2636cb127ce7afcd2aa1ff5bac9a5b96
krutoo <dima.krutoo@yandex.ru>
2023-06-30 12:22:54 +0500

    #38
    - metrics: раздел удален (функционал перенесен в пресеты из-за специфичности) (major)

--- src/metrics/constants.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Константы лейблов для метрик по соглашению.
- * @todo Убрать в пресеты.
- */
-export const ConventionalLabels = {
-  HTTP_RESPONSE: ['version', 'route', 'code', 'method'],
-  SSR: ['version', 'route', 'method'],
-} as const;
