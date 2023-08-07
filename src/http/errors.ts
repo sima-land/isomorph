6d588fa1a5e6dc73436e85e6651b7c525a044d69
krutoo <dima.krutoo@yandex.ru>
2023-08-07 16:41:17 +0500

    #38
    - http: добавлена ошибка ResponseError
    - http-server: SSRError помечена как deprecated

--- src/http/errors.ts
+++ src/http/errors.ts
@@ -1,5 +1,6 @@
 /**
  * Ошибка валидации статуса ответа.
+ * @todo Переименовать в HttpStatusValidationError?
  */
 export class StatusError extends Error {
   readonly response: Response;
@@ -20,5 +21,22 @@ export class StatusError extends Error {
    */
   static is(value: unknown): value is StatusError {
     return (value as any)?.name === 'StatusError';
+  }
+}
+
+/**
+ * Ошибка в процессе формирования ответа.
+ */
+export class ResponseError extends Error {
+  statusCode: number;
+
+  /**
+   * @param message Сообщение.
+   * @param statusCode Код ответа.
+   */
+  constructor(message: string, statusCode = 500) {
+    super(message);
+    this.name = 'ResponseError';
+    this.statusCode = statusCode;
   }
 }
