394cf216d442560d187077be75a376ba9758ab17
Dmitry Petrov <petrov_dm@ekb.sima-land.ru>
2023-10-12 16:44:25 +0500

    #38
    - попытка исправить работу TS с пакетом (не видит экспортов если они ссылаются на каталог с index.js)

---
+++ scripts/post-build-pkg.mjs
@@ -0,0 +1,33 @@
+import fs from 'fs-extra';
+import path from 'node:path';
+
+fs.readFile('./package.json', 'utf-8')
+  .then(JSON.parse)
+  .then(pkg => ({
+    pkg,
+    subfolders: Object.entries(pkg.exports)
+      .map(([key, value]) => ({
+        pathname: key,
+        definition: value,
+      }))
+      .filter(
+        ({ pathname, definition }) =>
+          path.basename(pathname) !==
+          path.basename(definition.import, path.extname(definition.import)),
+      ),
+  }))
+  .then(({ pkg, subfolders }) =>
+    Promise.all(
+      subfolders.map(item =>
+        fs.outputFile(
+          path.join(path.relative('./', item.pathname), 'package.json'),
+          JSON.stringify({
+            name: path.join(pkg.name, item.pathname),
+            types: path.relative(path.relative('./', item.pathname), item.definition.types),
+            main: path.relative(path.relative('./', item.pathname), item.definition.require),
+            module: path.relative(path.relative('./', item.pathname), item.definition.import),
+          }),
+        ),
+      ),
+    ),
+  );
