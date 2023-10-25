cc1361f3522b6248e96dc8b4ef9fec07acd5c370
Dmitry Petrov <petrov_dm@ekb.sima-land.ru>
2023-10-25 13:14:36 +0500

    #38
    - правки сборки пакета
    - обновлен @krutoo/fetch-tools

--- scripts/make-pkg-cjs.mjs
+++ scripts/make-pkg-cjs.mjs
@@ -3,6 +3,14 @@ import fs from 'fs-extra';
 
 fs.readFile('./package.json', 'utf-8')
   .then(JSON.parse)
-  .then(({ name, version }) => ({ name, version, type: 'commonjs' }))
+  .then(({ name, version, dependencies, peerDependencies }) => ({
+    type: 'commonjs',
+
+    // ВАЖНО: копируем эти поля для того чтобы устранить warning'и ModuleFederationPlugin
+    name,
+    version,
+    dependencies,
+    peerDependencies,
+  }))
   .then(data => fs.outputFile('dist/cjs/package.json', JSON.stringify(data, null, 2)))
   .then(() => console.log('[ui-quarks] CJS pkg emit done'));
