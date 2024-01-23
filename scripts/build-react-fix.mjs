6d768d7292f3696a264c2f54c9249f53c2364ad5
krutoo <dima.krutoo@yandex.ru>
2024-01-23 18:49:43 +0500

    esm-only
    - теперь собирается только ESM в папку dist
    - обновлен redux toolkit до 2 версии

--- scripts/build-react-fix.mjs
+++ scripts/build-react-fix.mjs
@@ -18,11 +18,18 @@ async function prependFile(path, content) {
 
 const react = `import * as React from 'react';\n`;
 
-glob('./dist/esm/**/*.js')
+glob('./dist/**/*.js')
   .then(items =>
     Promise.all(
       items.map(item => fs.readFile(item, 'utf-8').then(content => ({ filename: item, content }))),
     ),
   )
-  .then(files => files.filter(item => item.content.includes('React.createElement')))
+  .then(files =>
+    files.filter(
+      item =>
+        item.content.includes('React.createElement') &&
+        !item.content.includes('import React') &&
+        !item.content.includes('import * as React'),
+    ),
+  )
   .then(files => Promise.all(files.map(file => prependFile(file.filename, react))));
