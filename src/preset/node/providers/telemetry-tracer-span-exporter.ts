661b482fa2c999063d280097be367c7e9d90c25b
Ivan Burkov <burkov_i@ekb.sima-land.ru>
2025-05-07 16:21:48 +0500

    feat(telemetry): заголовки и порт OTEL из env

--- src/preset/node/providers/telemetry-tracer-span-exporter.ts
+++ src/preset/node/providers/telemetry-tracer-span-exporter.ts
@@ -15,9 +15,13 @@ export function provideSpanExporter(resolve: Resolve): OTLPTraceExporter {
   const headers = source.has(`${envPrefix}HEADERS`)
     ? JSON.parse(source.get(`${envPrefix}HEADERS`, "{}"))
     : undefined;
+  const url = new URL(source.get(`${envPrefix}URL`, "http://localhost:4317"));
+
+  if (source.has(`${envPrefix}PORT`))
+    url.port = source.get(`${envPrefix}PORT`, "");
 
   return new OTLPTraceExporter({
-    url: source.get(`${envPrefix}URL`, ""),
+    url: url.toString(),
     headers,
   });
 }
