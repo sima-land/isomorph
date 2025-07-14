ee7c7ff381bf7b725fd5ad7e9d00803b26e1c9f4
Ivan Burkov <burkov_i@ekb.sima-land.ru>
2025-07-14 15:27:41 +0500

    fix(otel): убран url в пользу шаблонной строки
    feat(otel): переменная hostname

--- src/preset/node/providers/telemetry-tracer-span-exporter.ts
+++ src/preset/node/providers/telemetry-tracer-span-exporter.ts
@@ -1,9 +1,9 @@
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
-import { KnownToken } from '../../../tokens';
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
+import { KnownToken } from "../../../tokens";
 
-import type { Resolve } from '../../../di';
+import type { Resolve } from "../../../di";
 
-const envPrefix = 'OTEL_EXPORTER_OTLP_';
+const envPrefix = "OTEL_EXPORTER_OTLP_";
 
 /**
  * Провайдер объекта SpanExporter.
@@ -13,14 +13,14 @@ const envPrefix = 'OTEL_EXPORTER_OTLP_';
 export function provideSpanExporter(resolve: Resolve): OTLPTraceExporter {
   const source = resolve(KnownToken.Config.source);
   const headers = source.has(`${envPrefix}HEADERS`)
-    ? JSON.parse(source.get(`${envPrefix}HEADERS`, '{}'))
+    ? JSON.parse(source.get(`${envPrefix}HEADERS`, "{}"))
     : undefined;
-  const url = new URL(source.get(`${envPrefix}URL`, 'http://localhost:4317'));
-
-  if (source.has(`${envPrefix}PORT`)) url.port = source.get(`${envPrefix}PORT`, '');
 
   return new OTLPTraceExporter({
-    url: url.toString(),
+    url: source.get(
+      `${envPrefix}URI`,
+      `${source.get(`${envPrefix}HOSTNAME`, "localhost")}:${source.get(`${envPrefix}PORT`, "4317")}`,
+    ),
     headers,
   });
 }
