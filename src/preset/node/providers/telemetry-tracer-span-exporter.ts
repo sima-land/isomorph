e8587d024ec06b4c94a978f1efb83474a92f98ba
Ivan Burkov <burkov_i@ekb.sima-land.ru>
2025-07-22 16:33:01 +0500

    fix(prettier): поправлен prettier

--- src/preset/node/providers/telemetry-tracer-span-exporter.ts
+++ src/preset/node/providers/telemetry-tracer-span-exporter.ts
@@ -15,7 +15,7 @@ export function provideSpanExporter(resolve: Resolve): OTLPTraceExporter {
   const headers = source.has(`${envPrefix}REQUEST_HEADERS`)
     ? JSON.parse(source.get(`${envPrefix}REQUEST_HEADERS`, '{}'))
     : undefined;
-  
+
   const url = new URL(source.get(`${envPrefix}ENTRYPOINT`) || `${source.get(`${envPrefix}PROTOCOL`, 'https')}://${source.get(`${envPrefix}HOSTNAME`, 'localhost')}`);
   url.port = source.get(`${envPrefix}PORT`, '4317');
 
